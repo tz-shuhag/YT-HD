d341847db36de5e1f27c5ce699b29f99733c8039
TZ Shuhag <106764078+tz-shuhag@users.noreply.github.com>
2025-05-31 03:38:37 +0600

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,5 @@
 chrome.runtime.onInstalled.addListener(function(details) {
-  if (details.reason == "install") {
-    chrome.tabs.create({
-        url: 'https://tz-shuhag.github.io/youtube-auto-hd/'
-    });
-  } else if (details.reason == "update") {
-    var version = chrome.runtime.getManifest().version;
-    chrome.tabs.create({
-        url: 'https://tz-shuhag.github.io/youtube-auto-hd/'
-    });
+  if (details.reason === "install" || details.reason === "update") {
+    chrome.tabs.create({ url: 'https://tz-shuhag.github.io/youtube-auto-hd/' });
   }
 });
