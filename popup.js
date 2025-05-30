92efd5639b25fa04484a004b969f1c66910e4782
TZ Shuhag <106764078+tz-shuhag@users.noreply.github.com>
2025-05-31 04:01:30 +0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,14 +1,19 @@
 (function() {
+    var version                 = chrome.runtime.getManifest().version;
     var preferredQualityElement = document.getElementById('preferred-quality');
 
+    var headerElem = document.querySelector('header .version');
+    headerElem.innerText = 'v' + version;
+
     chrome.storage.sync.get(['preferredQuality'], function(result) { 
-        if (result.preferredQuality !== undefined) {
+        if(result.preferredQuality !== undefined) {
             preferredQualityElement.value = result.preferredQuality;
         }
     });
 
     preferredQualityElement.onchange = function() {
         var selectedString = preferredQualityElement.options[preferredQualityElement.selectedIndex].value;
+
         chrome.storage.sync.set({preferredQuality: selectedString}, function() {});
     }
 })();
